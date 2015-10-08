d5d8d7142559f19c833c4df480ad89a6a2663aef
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-10-08 18:03:23 +0900

    create messages

---
+++ app/views/messages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @message, :id, :title, :body, :RAILS_ENV=development, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151008090205) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
 end
